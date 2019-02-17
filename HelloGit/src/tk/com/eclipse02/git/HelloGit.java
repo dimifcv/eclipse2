292263e7b2e80d57ae826dca30eda192863540a2
Damian Fernandez Condori <dimifcv@gmail.com>
2019-02-17 10:58:36 +0100

    Primer commit del proyecto HelloGit

---
+++ HelloGit/src/tk/com/eclipse02/git/HelloGit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package tk.com.eclipse02.git;
+
+/**
+ * @author damian
+ * Descripción: Utiliza git y eclipse
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world Git!");
+
+	}
+
+}
